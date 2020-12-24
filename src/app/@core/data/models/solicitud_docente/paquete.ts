2111c97575f393ed35f45c48304d23d9c665e359
CrissUD <cfpatinoc@correo.udistrital.edu.co>
2020-12-24 13:32:31 -0500

    feat: se crea modulo para revision de paquetes creando componente para lista de paquetes y lista de solicitudes dentro del paquete

--- src/app/@core/data/models/solicitud_docente/paquete.ts
+++ src/app/@core/data/models/solicitud_docente/paquete.ts
@@ -2,6 +2,7 @@ import { EstadoTipoSolicitud } from "./estado_tipo_solicitud";
 import { SolicitudDocentePost } from "./solicitud_docente";
 
 export class PaqueteSolicitudPost {
+  Id: number;
   Nombre: string;
   NumeroComite: string;
   FechaComite: string;
