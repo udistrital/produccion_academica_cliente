f6f3b1697d0d844cb8a332cb3af477b680f12ae5
CamiloQui <camiloquirogam@outlook.com>
2021-02-05 11:34:26 -0500

    feat: ajustes modulo y asignacion de rol evaluador al nuevo inscrito

--- src/app/pages/info_persona/crud-info-complementario/crud-info-complementario.component.ts
+++ src/app/pages/info_persona/crud-info-complementario/crud-info-complementario.component.ts
@@ -2,7 +2,7 @@ import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {BodyOutputType, Toast, ToasterConfig, ToasterService} from 'angular2-toaster';
 import Swal from 'sweetalert2';
 import {TranslateService, LangChangeEvent} from '@ngx-translate/core';
-import { UserService } from '../../../@core/data/users.service';
+import {UserService} from '../../../@core/data/users.service';
 import {HttpErrorResponse} from '@angular/common/http';
 import {FORM_INFO_COMPLEMENTARIA} from './form-info_complementaria';
 import {PopUpManager} from '../../../managers/popUpManager';
@@ -32,7 +32,7 @@ export class CrudInfoComplementarioComponent implements OnInit {
 
   @Input('info_complementaria_id')
   set persona(info_complementaria_id: number) {
-      this.loadInfoComplementaria();
+    this.loadInfoComplementaria();
   }
 
   @Output() eventChange = new EventEmitter();
@@ -44,10 +44,10 @@ export class CrudInfoComplementarioComponent implements OnInit {
   formInfoComplementaria: any;
   granAreaConocimiento: any;
   areaConocimientoEspecifica: any;
-  listGranAreaConocimiento= [];
-  listAreaConocimientoEspecifica= [];
-  listAreaConocimientoEspecificaFilter= [];
-  listNivelFormacion= [];
+  listGranAreaConocimiento = [];
+  listAreaConocimientoEspecifica = [];
+  listAreaConocimientoEspecificaFilter = [];
+  listNivelFormacion = [];
   nivelFormacion: any;
   institucion: any;
   info_inscripcion: any;
@@ -69,47 +69,19 @@ export class CrudInfoComplementarioComponent implements OnInit {
   userData: Tercero;
   userNum: string;
 
-  public loadInfoComplementaria(): void {
-    this.loading = true;
-     this.parametrosCrudService.get('parametro?query=TipoParametroId__Id:3&limit=0')
+  public loadInfoComplementaria(): Promise<any> {
+    return new Promise((resolve, reject) => {
+      this.loading = true;
+      this.parametrosCrudService.get('parametro?query=TipoParametroId__Id:3&limit=0')
         .subscribe(res => {
             this.info_info_complementario = res.Data;
-              for (let i = 0; i < this.info_info_complementario.length; i++) {
-                if (res.Data[i].ParametroPadreId == null) {
-                 this.listGranAreaConocimiento.push(res.Data[i]);
-                }else {
-                  this.listAreaConocimientoEspecifica.push(res.Data[i]);
-                }
+            for (let i = 0; i < this.info_info_complementario.length; i++) {
+              if (res.Data[i].ParametroPadreId == null) {
+                this.listGranAreaConocimiento.push(this.info_info_complementario[i]);
+              } else {
+                this.listAreaConocimientoEspecifica.push(this.info_info_complementario[i]);
               }
-              this.proyectoAcademicoService.get('nivel_formacion')
-                .subscribe( resp => {
-                  this.info_info_complementario = resp;
-                  for (let i = 0; i < this.info_info_complementario.length; i++) {
-                    if (resp[i].NivelFormacionPadreId != null) {
-                      this.listNivelFormacion.push(resp[i]);
-                    }
-                  }
-                    this.tercerosService.get('tercero/?query=Id:' + (this.user.getPersonaId() || 1))
-                      .subscribe(rest => {
-                        // if (res !== null) {
-                        if (Object.keys(rest[0]).length > 0) {
-                          this.userData = <Tercero>rest[0];
-                          this.userData['PuedeBorrar'] = false;
-                        }
-                      }, (error: HttpErrorResponse) => {
-                      });
-                },
-                  (error: HttpErrorResponse) => {
-                    Swal({
-                      type: 'error',
-                      title: error.status + '',
-                      text: this.translate.instant('ERROR.' + error.status),
-                      footer: this.translate.instant('GLOBAL.cargar') + '-' +
-                        this.translate.instant('GLOBAL.info_persona'),
-                      confirmButtonText: this.translate.instant('GLOBAL.aceptar'),
-                    });
-                  })
-              const files = []
+            }
           },
           (error: HttpErrorResponse) => {
             Swal({
@@ -120,54 +92,166 @@ export class CrudInfoComplementarioComponent implements OnInit {
                 this.translate.instant('GLOBAL.info_persona'),
               confirmButtonText: this.translate.instant('GLOBAL.aceptar'),
             });
+            reject(error);
           });
+      this.proyectoAcademicoService.get('nivel_formacion')
+        .subscribe(resp => {
+            this.info_info_complementario = resp;
+            for (let i = 0; i < this.info_info_complementario.length; i++) {
+              if (resp[i].NivelFormacionPadreId != null) {
+                this.listNivelFormacion.push(resp[i]);
+              }
+            }
+          },
+          (error: HttpErrorResponse) => {
+            Swal({
+              type: 'error',
+              title: error.status + '',
+              text: this.translate.instant('ERROR.' + error.status),
+              footer: this.translate.instant('GLOBAL.cargar') + '-' +
+                this.translate.instant('GLOBAL.info_persona'),
+              confirmButtonText: this.translate.instant('GLOBAL.aceptar'),
+            });
+            reject(error);
+          })
+      this.tercerosService.get('tercero/?query=Id:' + (this.user.getPersonaId()))
+        .subscribe(rest => {
+          // if (res !== null) {
+          if (Object.keys(rest[0]).length > 0) {
+            this.userData = <Tercero>rest[0];
+            this.userData['PuedeBorrar'] = false;
+          }
+        }, (error: HttpErrorResponse) => {
+          Swal({
+            type: 'error',
+            title: error.status + '',
+            text: this.translate.instant('ERROR.' + error.status),
+            footer: this.translate.instant('GLOBAL.cargar') + '-' +
+              this.translate.instant('GLOBAL.info_persona'),
+            confirmButtonText: this.translate.instant('GLOBAL.aceptar'),
+          });
+          reject(error);
+        });
+      resolve(true);
+    });
   }
+
   loadGrupinfoComplementaria(): void {
     this.tercerosService.get('info_complementaria/?query=Nombre:AREA_CONOCIMIENTO')
       .subscribe(res => {
         this.AREA_CONOCIMIENTO = res;
         this.tercerosService.get('info_complementaria/?query=Nombre:FORMACION_ACADEMICA')
           .subscribe(resp => {
-          this.NIVEL_FORMACION = resp
+            this.NIVEL_FORMACION = resp
             this.tercerosService.get('info_complementaria/?query=Nombre:INSTITUCION')
               .subscribe(rest => {
-               this.INSTITUCION = rest
+                this.INSTITUCION = rest
               }, (error: HttpErrorResponse) => {
+                Swal({
+                  type: 'error',
+                  title: error.status + '',
+                  text: this.translate.instant('ERROR.' + error.status),
+                  footer: this.translate.instant('GLOBAL.cargar') + '-' +
+                    this.translate.instant('GLOBAL.Informacion_complementaria'),
+                  confirmButtonText: this.translate.instant('GLOBAL.aceptar'),
+                });
               });
           }, (error: HttpErrorResponse) => {
+            Swal({
+              type: 'error',
+              title: error.status + '',
+              text: this.translate.instant('ERROR.' + error.status),
+              footer: this.translate.instant('GLOBAL.cargar') + '-' +
+                this.translate.instant('GLOBAL.Informacion_complementaria'),
+              confirmButtonText: this.translate.instant('GLOBAL.aceptar'),
+            });
           });
       }, (error: HttpErrorResponse) => {
+        Swal({
+          type: 'error',
+          title: error.status + '',
+          text: this.translate.instant('ERROR.' + error.status),
+          footer: this.translate.instant('GLOBAL.cargar') + '-' +
+            this.translate.instant('GLOBAL.Informacion_complementaria'),
+          confirmButtonText: this.translate.instant('GLOBAL.aceptar'),
+        });
       });
   }
+
   loadInfoComplementariaTercero(): void {
+    if (this.user.getPersonaId() != null) {
     this.tercerosService.get('info_complementaria_tercero/?query=TerceroId__Id:' +
-      (this.user.getPersonaId() || 1) + ',InfoComplementariaId__Nombre:AREA_CONOCIMIENTO')
-      .subscribe(res => {
-        this.AREA_CONOCIMIENTO2 = res;
-        this.tercerosService.get('info_complementaria_tercero/?query=TerceroId__Id:'   +
-          (this.user.getPersonaId() ||  1 ) + ',InfoComplementariaId__Nombre:FORMACION_ACADEMICA')
-          .subscribe(resp => {
-            this.NIVEL_FORMACION2 = resp
-            this.listaFormacion = JSON.parse(this.NIVEL_FORMACION2[this.NIVEL_FORMACION2.length - 1].Dato)['NivelFormacion']
-            this.nivelFormacion = this.listNivelFormacion.find(value => value.Id === this.listaFormacion.Id)
-            this.tercerosService.get('info_complementaria_tercero/?query=TerceroId__Id:' +
-              (this.user.getPersonaId() || 1) + ',InfoComplementariaId__Nombre:INSTITUCION')
-              .subscribe(rest => {
-                this.INSTITUCION2 = rest
-                this.listaArea = JSON.parse(this.AREA_CONOCIMIENTO2[this.AREA_CONOCIMIENTO2.length - 1].Dato)
-                this.listaGranArea = this.listaArea['AreaConocimiento']['GranAreaConocimiento']
-                this.listaEspeArea = this.listaArea['AreaConocimiento']['AreaConocimientoEspecifica']
-                this.listaInsti = JSON.parse(this.INSTITUCION2[this.INSTITUCION2.length - 1].Dato)
-                this.institucion = this.listaInsti.Institucion
-                this.granAreaConocimiento =  this.listGranAreaConocimiento.find(value => value.Id === this.listaGranArea.Id)
-                this.filterAreaConocimiento(this.granAreaConocimiento)
-                this.areaConocimientoEspecifica = this.listAreaConocimientoEspecifica.find(value => value.Id === this.listaEspeArea.Id)
-              }, (error: HttpErrorResponse) => {
-              });
-          }, (error: HttpErrorResponse) => {
-          });
+      (this.user.getPersonaId()) + ',InfoComplementariaId__Nombre:FORMACION_ACADEMICA')
+      .subscribe(resp => {
+        if (resp !== null && resp !== 'error') {
+          this.NIVEL_FORMACION2 = resp
+          this.listaFormacion = JSON.parse(this.NIVEL_FORMACION2[this.NIVEL_FORMACION2.length - 1].Dato)['NivelFormacion']
+          this.nivelFormacion = this.listNivelFormacion.find(value => value.Id === this.listaFormacion.Id)
+          this.loadInfoGrupoAcademico()
+          // console.info(this.listNivelFormacion)
+          // console.info(this.listNivelFormacion.find(value => value.Id === this.listaFormacion.Id))
+        }
       }, (error: HttpErrorResponse) => {
+      //  Swal({
+      //    type: 'error',
+      //    title: error.status + '',
+      //    text: this.translate.instant('ERROR.' + error.status),
+      //    footer: this.translate.instant('GLOBAL.cargar') + '-' +
+      //      this.translate.instant('GLOBAL.Informacion_complementaria'),
+      //    confirmButtonText: this.translate.instant('GLOBAL.aceptar'),
+      //  });
       });
+    this.tercerosService.get('info_complementaria_tercero/?query=TerceroId__Id:' +
+      (this.user.getPersonaId()) + ',InfoComplementariaId__Nombre:INSTITUCION')
+      .subscribe(rest => {
+        if (rest !== null && rest !== 'error') {
+          this.INSTITUCION2 = rest
+          this.listaInsti = JSON.parse(this.INSTITUCION2[this.INSTITUCION2.length - 1].Dato)
+          this.institucion = this.listaInsti.Institucion
+        }
+      }, (error: HttpErrorResponse) => {
+      //  Swal({
+      //    type: 'error',
+      //    title: error.status + '',
+      //    text: this.translate.instant('ERROR.' + error.status),
+      //    footer: this.translate.instant('GLOBAL.cargar') + '-' +
+      //      this.translate.instant('GLOBAL.Informacion_complementaria'),
+      //    confirmButtonText: this.translate.instant('GLOBAL.aceptar'),
+      //  });
+      });
+  }
+  }
+  loadInfoGrupoAcademico(): void {
+    if (this.user.getPersonaId() != null) {
+      this.tercerosService.get('info_complementaria_tercero/?query=TerceroId__Id:' +
+        (this.user.getPersonaId()) + ',InfoComplementariaId__Nombre:AREA_CONOCIMIENTO')
+        .subscribe(res => {
+          if (res !== null && res !== 'error') {
+            this.AREA_CONOCIMIENTO2 = res;
+            this.listaArea = JSON.parse(this.AREA_CONOCIMIENTO2[this.AREA_CONOCIMIENTO2.length - 1].Dato)
+            this.listaGranArea = this.listaArea['AreaConocimiento']['GranAreaConocimiento']
+            this.listaEspeArea = this.listaArea['AreaConocimiento']['AreaConocimientoEspecifica']
+            this.granAreaConocimiento = this.listGranAreaConocimiento.find(value => value.Id = this.listaGranArea.Id)
+            console.info(this.listGranAreaConocimiento)
+            console.info(this.listAreaConocimientoEspecifica)
+            console.info(this.listaGranArea)
+            console.info(this.listaEspeArea)
+            console.info(this.listGranAreaConocimiento.find(value => this.listaGranArea.Id = value.Id))
+            this.filterAreaConocimiento(this.granAreaConocimiento)
+            this.areaConocimientoEspecifica = this.listAreaConocimientoEspecifica.find(value => value.Id === this.listaEspeArea.Id)
+            console.info(this.listAreaConocimientoEspecifica.find(value => value.Id === this.listaEspeArea.Id))
+          }
+        }, (error: HttpErrorResponse) => {
+          // Swal({
+          //   type: 'error',
+          // title: error.status + '',
+          //   text: this.translate.instant('ERROR.' + error.status),
+          //   footer: this.translate.instant('GLOBAL.cargar') + '-' +
+          //     this.translate.instant('GLOBAL.Informacion_complementaria'),
+          //   confirmButtonText: this.translate.instant('GLOBAL.aceptar'),
+          // });
+        });
+    }
   }
 
   constructor(private translate: TranslateService, private popUpManager: PopUpManager,
@@ -180,9 +264,21 @@ export class CrudInfoComplementarioComponent implements OnInit {
               private store: Store<IAppState>,
               private listService: ListService,
               private toasterService: ToasterService) {
-    this.loadInfoComplementaria();
-    this.loadGrupinfoComplementaria()
+    this.loadInfoComplementaria()
+      .then(() => {})
+      .catch(error => {
+        if (!error.status) {
+          error.status = 409;
+        }
+        Swal({
+          type: 'error',
+          title: error.status + '',
+          text: this.translate.instant('ERROR.' + error.status),
+          confirmButtonText: this.translate.instant('GLOBAL.aceptar'),
+        });
+      });
     this.loadInfoComplementariaTercero()
+    this.loadGrupinfoComplementaria()
     this.formInfoComplementaria = JSON.parse(JSON.stringify(FORM_INFO_COMPLEMENTARIA));
     this.construirForm();
     this.translate.onLangChange.subscribe((event: LangChangeEvent) => {
@@ -280,11 +376,13 @@ export class CrudInfoComplementarioComponent implements OnInit {
   validarForm(event) {
     this.createInfoComplementaria()
   }
+
   validCampo(event): boolean {
     if (event == null) {
       return false;
     }
   }
+
   filterAreaConocimiento(granAreaConocimiento: any) {
     if (granAreaConocimiento == null) {
       return false;
@@ -293,10 +391,12 @@ export class CrudInfoComplementarioComponent implements OnInit {
         .filter(AreaConocimientoEspecifica => AreaConocimientoEspecifica.ParametroPadreId.Id === granAreaConocimiento.Id);
     }
   }
+
   setPercentage(event) {
     this.percentage = event;
     this.result.emit(this.percentage);
   }
+
   private showToast(type: string, title: string, body: string) {
     this.config = new ToasterConfig({
       // 'toast-top-full-width', 'toast-bottom-full-width', 'toast-top-left', 'toast-top-center'
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -62,4 +62,5 @@ export const environment = {
   CIDC_SERVICE: 'http://200.69.103.88:3114/api/v1/',
   EVALUACION_INSCRIPCION_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/evaluacion_inscripcion_crud/v2/',
   INSCRIPCION_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/inscripcion_crud/v2/',
+  AUTENTICACION_MID_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/autenticacion_mid/v1/',
 };
